bb2d80e9e9834151839579b34404263fc4ffd3b1
adrantine <adrantine@gmail.com>
2026-08-24 10:41:42 +0000

    fix: shrink QC report logo further and add top clearance
    height: 50px -> 34px with display: block (drops the inline-image's
    baseline gap) plus 8px padding-bottom on the cell, so the logo sits
    comfortably clear of the header's border-bottom line.

--- resources/views/qc/pdf/partials/inspection-report-body.blade.php
+++ resources/views/qc/pdf/partials/inspection-report-body.blade.php
@@ -46,9 +46,9 @@
 {{-- Header --}}
 <table style="border-bottom: 1.5px solid #0F172A; margin-bottom: 5px;">
     <tr>
-        <td style="width: 45%; vertical-align: top;">
+        <td style="width: 45%; vertical-align: top; padding-bottom: 8px;">
             @if ($logoData)
-                <img src="{{ $logoData }}" style="height: 50px;">
+                <img src="{{ $logoData }}" style="height: 34px; display: block;">
             @endif
         </td>
         <td style="width: 55%; text-align: right; vertical-align: bottom; padding-bottom: 3px;">
